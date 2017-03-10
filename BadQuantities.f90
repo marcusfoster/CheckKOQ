311d6510e05b61a208c02cb1b1da11952c0e0632
marcusfoster <marcus.foster@outlook.com>
2017-03-10 23:16:07 +1100

    Update BadQuantities.f90

--- BadQuantities.f90
+++ BadQuantities.f90
@@ -1,22 +1,21 @@
-program BadQuantities
+program BadQuantities1
 implicit none 
 ! rotating flywheel with torque applied for a duration
 ! find the initial and final kinetic energy
-!= KOQRelation :: ROTENERGY = AV ** 2 * MOI
-!= KOQRelation :: AV = TORQUE / MOI * TIME
-!= KOQ MOI :: I
-!= KOQ AV :: ang_vel_init, ang_vel_final
-!= KOQ TORQUE :: torque
-!= KOQ TIME :: duration
-!= KOQ ROTENERGY :: energy_init, energy_final
+! annotated with Camfort units declarations
+!= unit kg*m**2 :: I
+!= unit kg m**2/s**2 :: torque
+!= unit s :: duration
+!= unit 1/s :: ang_vel_init, ang_vel_final
+!= unit kg m**2/s**2 :: energy_init, energy_final
 real, parameter :: I = 6.0, ang_vel_init = 7.0, torque = 8.0, duration = 9.0 
 real ::  energy_init, energy_final, ang_vel_final
 energy_init = 0.5 * I * ang_vel_init ** 2
-! unit-correctness ALLOWS, but kind-of-quantity-correctness DETECTS, this meaningless quantity addition:
+! unit correctness, allows this meaningless quantity addition:
 energy_final = energy_init + torque
-! unit-correctness ALLOWS, but kind-of-quantity correctness DETECTS, this meaningless quantity multiplication:
+! unit correctness, allows this meaningless quantity multiplication:
 energy_final = I / duration ** 2
-! correct algorithm for finding final angular velocity, then rotational energy
+! correct algorithm for finding final angular velocity, then kinetic energy
 ang_vel_final = ang_vel_init + torque / I * duration
 energy_final = 0.5 * I * ang_vel_final ** 2
-end program BadQuantities
+end program BadQuantities1
